ad1a7b02b7a0bb633c40d72c0e59945bb6b41df7
Codel1417 <austinrichards98@gmail.com>
2020-01-09 18:46:20 -0700

    Update Configuration.h

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -909,7 +909,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -.42 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.295‬ }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 25
